efff6bfaca2a60752a106932e16160fb4c89b70a
Kosmogen <gen.raiyama@gmail.com>
2020-12-13 23:31:04 -0600

    Optimized to only generated particles around model width

--- particle_chamber.py
+++ particle_chamber.py
@@ -10,9 +10,10 @@ from particle_emitter import ParticleEmitter
 class ParticleChamber:
     """Contains all the variables and methods for running the main game loop."""
     def __init__(self):
-        self.FPS = 30
+        self.FPS = 25
         self.WINDOW_RES = (640, 480)
         self.BG_COLOR = (0, 0, 0) # RGB for black
+        self.particle_x_step_res = 5
 
         pygame.init()
         self.FramePerSec = pygame.time.Clock()
@@ -21,13 +22,14 @@ class ParticleChamber:
     def game_loop(self):
         """Main game loop."""
         # fixed sprites
-        self.player_ship = Model(*self.WINDOW_RES, 'player_ship.png')
-        self.player_ship.update()
+        self.model = Model(*self.WINDOW_RES, 'player_ship.png')
+        self.model.update()
 
         # Particle emitters
         self.particles = pygame.sprite.Group()
         self.emitters = []
-        for x_col in range(0, self.WINDOW_RES[0], 5):
+        for x_col in range(self.model.rect.left - (self.particle_x_step_res * 3), 
+                            self.model.rect.right + (self.particle_x_step_res * 3), self.particle_x_step_res):
             emitter = ParticleEmitter(*self.WINDOW_RES, x_col, self.particles)
             self.emitters.append(emitter)
 
@@ -43,18 +45,18 @@ class ParticleChamber:
             # Update sprites
             for emitter in self.emitters:
                 emitter.update()
-            self.player_ship.update()
+            self.model.update()
             for particle in self.particles:
                 particle.update()
 
             # Draw sprites
             self.DISPLAYSURF.fill(self.BG_COLOR)
-            self.player_ship.draw(self.DISPLAYSURF)
+            self.model.draw(self.DISPLAYSURF)
             for particle in self.particles:
                 particle.draw(self.DISPLAYSURF)
 
             # Detect collision between player and any asteroids
-            if pygame.sprite.spritecollideany(self.player_ship, self.particles, pygame.sprite.collide_mask):
+            if pygame.sprite.spritecollideany(self.model, self.particles, pygame.sprite.collide_mask):
                 # TODO: animate particle bounce
                 pass
 
